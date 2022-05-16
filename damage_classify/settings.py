d424198f3d3d7c67c6783d9d59b5bedf4b71111b
Vishwanath Kannan <vishwanathkannanbsb@gmail.com>
2022-05-16 22:39:26 +0530

    allowed hosts updated

--- damage_classify/settings.py
+++ damage_classify/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-0qo0r0&$!tpl(kqvy+i@b#%ujujnafng@(3dkx)z--*b%d^0w4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
